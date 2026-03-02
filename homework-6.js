9bc89d3cb9f91ab0cd52d24923fb10bce2cc7846
apex-h7 <julie.sim@mail.ru>
2026-03-02 14:13:50 +0300

    feature/06-homework

---
+++ homework-6.js
@@ -0,0 +1,117 @@
+// Пункт 3. Объект с моими данными
+
+const profile = {
+  name: "Hadidzha",
+  age: 39,
+  country: "Belarus",
+  city: "Minsk",
+  maritalStatus: "divorced",
+  children: 6
+};
+
+
+// Пункт 4. Объект с данными об автомобиле и доп свойством
+
+const carData = {
+  brand: "Toyota",
+  model: "Land Cruiser",
+  year: 2026,
+  color: "Brown",
+  transmission: "Automatic",
+};
+
+carData.owner = profile;
+
+
+// Пункт 5. Написать функцию, принимающую авргкментом объектом пункт 4
+
+const checkSpeed = (carData) => {
+  if (!carData.maxSpeed) {
+    carData.maxSpeed = "200км/ч";
+  }
+};
+
+
+// Пункт 6. Написать функцию, которая получает первым аргументом - объект, 
+// вторым-свойство
+
+const getProperty = (obj, key) => {
+  console.log(obj [key]);
+};
+
+
+// Пункт 7. Создать массив, с названиями продруктов
+
+const products = [ "Шелк", "Хлопок", "Барахат" ];
+
+
+// Пункт 8. Создать массив из обьектов 
+
+const classicsBooks = [
+  {
+    title: "Портрет Дориана Грея",
+    author: "Оскар Уайлд",
+    year: 1890,
+    genre: "Роман"
+  },
+  {
+    title: "Фауст",
+    author: "Иоганн Гете",
+    year: 1808,
+    genre: "Трагедия"
+  },
+  {
+    title: "Сто лет одиночества",
+    author: "Габриэль Маркес",
+    year: 1967,
+    genre: "Роман"
+  }
+];
+
+classicsBooks.push({
+  title: "Старик и море",
+  author: "Эрнест Хемингуэй",
+  year: 1952,
+  genre: "Повесть"
+});
+
+
+// Пункт 9. Создание еще одного массива и объединение 
+//двух массивов в один
+
+const scienceFictionBooks = [
+  {
+    title: "Путешествие к центру Земли",
+    author: "Жюль Верн",
+    year: 1864,
+    genre: "Научная фантастика"
+  },
+  {
+    title: "451 градус по Фаренгейту",
+    author: "Рэй Бредбери",
+    year: 1953,
+    genre: "Научная фантастика"
+  }];
+ 
+  const library = [ ...classicsBooks, ...scienceFictionBooks];
+  
+  
+  // Пункт 10. Написать функцию, которая принимает массив 
+  // сущностей с пункта 9 
+
+  const markRareBooks = (booksArray) => {
+    return booksArray.map((book) => {
+      let rareStatus;
+      if (book.year > 2000) {
+        rareStatus = true;
+      } else {
+        rareStatus  = false;
+      }
+      return {
+        ...book,
+        isRare: rareStatus,
+      };
+    });
+  };
+
+  const updatedLibrary = markRareBooks(library);
